88a7bc8cd50a2d28d9a9b76c89dce07d155bd1b2
Valentinus Delvin <delvinwicaksono@gmail.com>
2024-10-13 23:21:49 +0700

    Add files via upload

---
+++ KalkulatorLegend.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class KalkulatorLegend {
+    public static void main(String[] args) {
+        double awal = 0;
+        
+        Scanner damai = new Scanner(System.in);
+        
+        
+    }
+}
