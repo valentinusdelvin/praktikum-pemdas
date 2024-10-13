e52a22e64efcd4421f74948da5df4322ad346a67
Valentinus Delvin <delvinwicaksono@gmail.com>
2024-10-13 23:24:24 +0700

    Update KalkulatorLegend.java

--- KalkulatorLegend.java
+++ KalkulatorLegend.java
@@ -1,11 +1,41 @@
 import java.util.*;
-
 public class KalkulatorLegend {
     public static void main(String[] args) {
-        double awal = 0;
+    Scanner damai = new Scanner (System.in);
+    double akhir = 0.0;
         
-        Scanner damai = new Scanner(System.in);
-        
-        
+        while (true){
+            String ops = damai.next();
+            double angka = damai.nextDouble();
+
+            if (angka == 0){
+                break;
+            }
+
+            switch (ops) {
+                case "TAMBAH":
+                    akhir += angka;
+                    break;
+                case "KURANG":
+                    akhir -= angka;
+                    break;
+                case "KALI":
+                    akhir *= angka;
+                    break;
+                case "BAGI":
+                    akhir /= angka;
+                    break;
+            } 
+            
+        }
+        if (akhir <0){
+            akhir += 10;
+        } else if (akhir == 0){
+            akhir +=20;
+        } else if (akhir > 0){
+            akhir*=akhir;
+        }
+
+        System.out.printf("%.1f\n",akhir);
     }
 }
